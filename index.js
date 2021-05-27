b1430f1d3274ddadbe58bd4c4515ec9c75159e71
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-05-27 12:09:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+greet("Brent");
+
+function person(name, lastName) {
+  console.log("Hello " + name + " " + lastName);
+}
+
+person("Brent", "Johnson");
